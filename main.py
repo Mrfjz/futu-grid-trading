f5570d95d18ba80e27407ee56307eee016fb1c9d
Chris Fu <chaosfjz@hotmail.com>
2021-10-08 09:39:23 +0800

    fix: add DRY_RUN for sell market order

--- main.py
+++ main.py
@@ -135,14 +135,20 @@ def main(args):
             # place sell order for equity
             if order_quantity < 0:
                 logger.info(f"Placing sell market order, symbol={SYMBOL}, quantity={abs(order_quantity)}")
-                place_sell_market_order(SYMBOL, abs(order_quantity))
-                logger.info("Order placed")
+                if DRY_RUN:
+                    logger.debug("Dry run is on, do not process")
+                else:
+                    place_sell_market_order(SYMBOL, abs(order_quantity))
+                    logger.info("Order placed")
 
             # place sell order for inverse equity
             if ie_order_quantity < 0:
                 logger.info(f"Placing sell market order, symbol={SYMBOL}, quantity={abs(order_quantity)}")
-                place_sell_market_order(IE_SYMBOL, abs(ie_order_quantity))
-                logger.info("Order placed")
+                if DRY_RUN:
+                    logger.debug("Dry run is on, do not process")
+                else:
+                    place_sell_market_order(IE_SYMBOL, abs(ie_order_quantity))
+                    logger.info("Order placed")
 
             # place buy order for equity
             if order_quantity > 0:
