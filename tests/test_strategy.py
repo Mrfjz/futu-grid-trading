1a4d14298b57b4280432bbb2f2e7360305094abe
Chris Fu <chaosfjz@hotmail.com>
2021-11-04 16:21:19 +0800

    feat: relocate test_connection.py, remove test_api.py

--- tests/test_strategy.py
+++ tests/test_strategy.py
@@ -1,28 +1,29 @@
 from trade.strategy import GridTradingStrategy
+import pytest
 
 
 def test_position_per_grid():
-    strategy = GridTradingStrategy(11, 10, 5, 10000, 20000)
+    strategy = GridTradingStrategy(11, 10, 5, 10000, 20000, 100, 100)
     assert strategy.position_per_grid == 2000
 
 
 def test_position_per_grid_cannot_be_divided():
-    strategy = GridTradingStrategy(11, 10, 15, 10000, 20000)
-    assert strategy.position_per_grid == 700
+    with pytest.raises(Exception):
+        strategy = GridTradingStrategy(11, 10, 15, 10000, 20000, 100, 100)
 
 
 def test_grids():
-    strategy = GridTradingStrategy(11, 10, 5, 10000, 20000)
+    strategy = GridTradingStrategy(11, 10, 5, 10000, 20000, 100, 100)
     assert strategy.grids == [10, 10.2, 10.4, 10.6, 10.8, 11]
 
 
 def test_grids_cannot_be_divided():
-    strategy = GridTradingStrategy(11, 10, 3, 10000, 20000)
-    assert strategy.grids == [10, 10.333, 10.667, 11]
+    with pytest.raises(Exception):
+        strategy = GridTradingStrategy(11, 10, 3, 10000, 20000, 100, 100)
 
 
 def test_cal_grid_index_by_price():
-    strategy = GridTradingStrategy(11, 10, 10, 10000, 20000)
+    strategy = GridTradingStrategy(11, 10, 10, 10000, 20000, 100, 100)
     assert strategy.cal_grid_index_by_price(9) == 0
     assert strategy.cal_grid_index_by_price(10) == 1
     assert strategy.cal_grid_index_by_price(10.9) == 10
@@ -30,7 +31,7 @@ def test_cal_grid_index_by_price():
 
 
 def test_cal_order_quantity():
-    strategy = GridTradingStrategy(11, 10, 10, 10000, 20000)
+    strategy = GridTradingStrategy(11, 10, 10, 10000, 20000, 100, 100)
     assert strategy.cal_order_quantity(9.95, 0, 0) == (10000, 0)
     assert strategy.cal_order_quantity(10.05, 0, 0) == (9000, 2000)
 
